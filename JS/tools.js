0f485022672a80ec1491c8481a601320e7038dd7
NestorManeiro <91387558+NestorManeiro@users.noreply.github.com>
2023-03-05 16:37:29 +0000

    Commit merge with jose

---
+++ JS/tools.js
@@ -0,0 +1,59 @@
+function mostrar(id){
+    id.style.display = "initial";
+}
+function ocultar(id){
+    id.style.display = "none";
+}
+function mostrar_features(){
+    ocultar(comments);
+    ocultar(questions);
+    ocultar(videos);
+    mostrar(features);
+}
+
+function mostrar_comments(){
+    ocultar(features);
+    ocultar(questions);
+    ocultar(videos);
+    mostrar(comments);
+}
+
+function mostrar_videos(){
+    ocultar(features);
+    ocultar(questions);
+    ocultar(comments);
+    mostrar(videos);
+}
+
+function mostrar_questions(){
+    ocultar(features);
+    ocultar(comments);
+    ocultar(videos);
+    mostrar(questions);
+}
+
+
+
+
+function continuar(id){
+    var div_cest = document.getElementById('cest');
+    var div_shipment_directions = document.getElementById('shipment-directions');
+    var div_delivery_options = document.getElementById('delivery-options');
+    var div_payment = document.getElementById('payment');
+    var div_resume = document.getElementById('resume');
+
+    const arrayIds = [div_cest, div_shipment_directions, div_delivery_options, div_payment, div_resume];
+    arrayIds.forEach(ocultar);
+
+    if(id == 'shipment-directions'){
+        div_shipment_directions.style.display = "inline-flex"
+    }
+    else if(id == 'resume'){
+        div_resume.style.display = "inline-flex"
+    }
+    else{
+        mostrar(document.getElementById(id));
+    }
+    
+}
+
