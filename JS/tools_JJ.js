d2f58b41c249a18dd8ec52011ece66f4a107b375
jjsmx24@gmail.com <jjsmx24@gmail.com>
2023-03-21 13:51:17 +0000

    json hecho

--- JS/tools_JJ.js
+++ JS/tools_JJ.js
@@ -1,8 +1,8 @@
-fetch('./json/usuario.json')
+fetch('json/usuario.json')
   .then(response => response.json())
   .then(data => {
     // Haz lo que quieras con los datos del archivo JSON
-    const p = document.getElementById('datos');
-    p.textContent = "Nick: " + data.Nick;
+    const p = document.getElementById("datos");
+    p.textContent = ""+data.Nick;
   });
 
