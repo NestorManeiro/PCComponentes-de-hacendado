cc1de09e60fad8b2f6f2aba222d6dc75af7dbd0c
jjsmx24@gmail.com <jjsmx24@gmail.com>
2023-03-25 20:03:53 +0000

    victoriaaaa

--- JS/tools_JJ.js
+++ JS/tools_JJ.js
@@ -151,3 +151,43 @@ carousel2.addEventListener("touchmove", dragging2);
 
 document.addEventListener("mouseup", dragStop2);
 carousel2.addEventListener("touchend", dragStop2);
+
+/*Separador*/
+
+"use strict"
+
+
+const carouselButtons = document.querySelectorAll(".carousel-btn");
+const carouselButtonLeft = document.querySelector(".btn-left");
+const carouselButtonRight = document.querySelector(".btn-right");
+const carouselImage1 = document.querySelector(".carousel-image-1");
+const carouselImage2 = document.querySelector(".carousel-image-2");
+const carouselImage3 = document.querySelector(".carousel-image-3");
+const numberOfImages = document.querySelectorAll(".carousel-image").length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtonLeft.addEventListener("click", slideLeft);
+carouselButtonRight.addEventListener("click", slideRight);
+
+function slideLeft(){
+    if (imageIndex !== 1){
+        imageIndex--;
+        translateX += 700;
+        carouselImage1.style.transform = `translateX(${translateX}px)`;
+        carouselImage2.style.transform = `translateX(${translateX}px)`;
+        carouselImage3.style.transform = `translateX(${translateX}px)`;
+    }
+    
+}
+
+function slideRight(){
+    if (imageIndex !== numberOfImages){
+        imageIndex++;
+        translateX -= 700;
+        carouselImage1.style.transform = `translateX(${translateX}px)`;
+        carouselImage2.style.transform = `translateX(${translateX}px)`;
+        carouselImage3.style.transform = `translateX(${translateX}px)`;
+    }
+    
+}
